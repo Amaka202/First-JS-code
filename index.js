f789fe0833268dd9613620a5bf2d88bcd7992807
Umeh Amaka <amakaumeh01@gmail.com>
2020-03-24 13:33:39 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let name = "Amaka202";
+
+var myCourses = ["HTML", "CSS", "JAVASCRIPT", "PHP"];
+
+console.log("My name is: " + name);
+
+console.log("These are my courses: " + myCourse);
+
+const data = [];
+
+for (let i = 0; i <= 200; i++) {
+
+    if (i % 2 === 0) {
+
+        data.push(i);
+
+    }
+
+}
+
+console.log(data);
